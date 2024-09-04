05db99b2eb86422edc9ae14f32889cc6f37e86cd
Fahad Ahmed <fahad838@gmail.com>
2024-09-04 19:30:39 +0530

    add functions to Board class
    Board class now has a @board when initialized
    which is an array that stores user inputs.
    it has a function show_board to show the previous
    input and, a function update board to update the
    board with the given user input.

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,21 @@
 class Board
+  attr_accessor :board
+
   def initialize
+    @board = []
+  end
+
+  def update_board(arr)
+    board.push(arr)
+  end
+
+  def show_board
+    p board
   end
 end
+
+board = Board.new
+
+board.update_board('YRGB')
+
+board.show_board
