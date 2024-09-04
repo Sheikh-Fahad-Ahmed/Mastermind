bc07ed5a3eccea3d099a17a81b52b9a700c0aa41
Fahad Ahmed <fahad838@gmail.com>
2024-09-04 09:53:38 +0530

    Create a menu for the user @display.rb
    Created a menu for the user, where the options are
    to Play game, Show the Colors, Instructions of the
    game and Exit

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,4 @@
 class Board
   def initialize
-    
   end
 end
--- lib/display.rb
+++ lib/display.rb
@@ -2,4 +2,12 @@ module Display
   def welcome_message
     puts 'Welcome to The Mastermind'
   end
+
+  def menu
+    puts '------Menu------'
+    puts '1. Play game'
+    puts '2. Show all colors'
+    puts '3. Instructions'
+    puts '4. Exit'
+  end
 end
