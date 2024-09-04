b85df0cb8927754e9f8ea458defbb718bfb15ef9
Fahad Ahmed <fahad838@gmail.com>
2024-09-04 17:58:52 +0530

    Create a start_game function when players picks 1
    Created a start_game function when players picks 1
    from start_menu it will show as generating secret
    code..

--- lib/colors.rb
+++ lib/colors.rb
@@ -6,7 +6,6 @@ class Colors
   end
 
   def generate_secret_code
-    @secret_code = @colors.sample(4)
+    @colors.sample(4)
   end
 end
-
--- lib/game.rb
+++ lib/game.rb
@@ -3,21 +3,38 @@
 require_relative 'display'
 
 class Game
+  attr_reader :all_colors, :secret_code
+
   include Display
+  def initialize
+    @all_colors = Colors.new
+  end
 
   def start
     welcome_message
-    start_menu
-    choice = gets.chomp
-    go_to_choice(choice)
+    loop do
+      start_menu
+      choice = gets.chomp.to_i
+      go_to_choice(choice)
+    end
   end
 
   def go_to_choice(choice)
     case choice
     when 1
-      'play game'
+      start_game
     when 2
-
+      puts @all_colors.colors.join(' ')
+    when 4
+      exit 0
     end
+  end
+
+  def start_game
+    puts "Generating secret code...\n.\n.\n."
+    @secret_code = @all_colors.generate_secret_code
+    puts 'Secret code generated'
+    puts "You get 12 turns...Let's Begin"
+    p @secret_code
   end
 end
